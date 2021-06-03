2df0adf5719681609bbe5e63d383fc1851ff4f87
Luis Chang <luis.chang@outlook.com>
2021-06-02 20:31:57 -0500

    Método grabar ajax

--- M4_NETCoreMVCJQUERY.DatabaseFirst/Controllers/CustomerController.cs
+++ M4_NETCoreMVCJQUERY.DatabaseFirst/Controllers/CustomerController.cs
@@ -17,10 +17,39 @@ namespace M4_NETCoreMVCJQUERY.DatabaseFirst.Controllers
 
         public async Task<IActionResult> Listado()
         {
-            var listado =await CustomerRepo.GetCustomersAsync();            
+            var listado = await CustomerRepo.GetCustomersAsync();
             return PartialView(listado);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Grabar(int idCliente,
+                                                string nombres,
+                                                string apellidos,
+                                                string pais,
+                                                string ciudad,
+                                                string telefono)
+        {
+            var customer = new Customer()
+            {
+                FirstName = nombres,
+                LastName = apellidos,
+                Country = pais,
+                City = ciudad,
+                Phone = telefono
+            };
+
+            bool exito = true;
+
+            if (idCliente == -1)
+                exito = await CustomerRepo.Insert(customer);
+            else
+            {
+                customer.Id = idCliente;
+                exito = await CustomerRepo.Update(customer);
+            }
+
+            return Json(exito);
+        }
 
 
     }
